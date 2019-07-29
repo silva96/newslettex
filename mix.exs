b073af3c9c1eaa06bb0539caa8c485b87acca51b
Benjamín Silva <silva96@gmail.com>
2019-07-28 23:00:27 -0400

    add excoveralls to project

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Newslettex.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
