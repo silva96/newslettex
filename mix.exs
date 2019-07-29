ae0e911f4d6f098756fb6b80dbb69150c6bfd876
Benjamín Silva <silva96@gmail.com>
2019-07-28 22:37:23 -0400

    add codecov

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Newslettex.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
+      {:excoveralls, "~> 0.11.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
